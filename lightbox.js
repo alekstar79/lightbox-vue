67fbd17524765d13f1e464dff2d0d7bb66ea23cd
alekstar79 <alekstar79@yandex.ru>
2023-07-18 17:45:35 +0400

    Integrating a renderer into a vue prototype

--- lightbox.js
+++ lightbox.js
@@ -1,123 +1,3 @@
-class Renderer
-{
-  static instance
-
-  static init = (options) => Renderer.instance ||= new Renderer(options)
-
-  static valueInRange = ({ minScale, maxScale, scale }) => scale <= maxScale && scale >= minScale
-
-  static hasPositionChanged = ({ pos, prevPos }) => pos !== prevPos
-
-  static getTranslate = ({ minScale, maxScale, scale }) => ({ pos, prevPos, translate }) =>
-    Renderer.valueInRange({ minScale, maxScale, scale }) &&
-    Renderer.hasPositionChanged({ pos, prevPos })
-      ? translate + (pos - prevPos * scale) * (1 - 1 / scale)
-      : translate
-
-  static getScale = ({ scale, minScale, maxScale, scaleSensitivity, deltaScale }) => {
-    let newScale = scale + (deltaScale / (scaleSensitivity / scale))
-
-    newScale = Math.max(minScale, Math.min(newScale, maxScale))
-
-    return [scale, newScale]
-  }
-
-  static getMatrix = ({ scale, translateX, translateY, skewX = 0, skewY = 0 }) =>
-    `matrix(${scale}, ${skewX}, ${skewY}, ${scale}, ${translateX}, ${translateY})`
-
-  constructor({ minScale, maxScale, element, scaleSensitivity = 10 })
-  {
-    this.state = {
-      element,
-      minScale,
-      maxScale,
-      scaleSensitivity,
-      transformation: {
-        translateX: 0,
-        translateY: 0,
-        originX: 0,
-        originY: 0,
-        scale: 1
-      }
-    }
-  }
-
-  zoom({ x, y, deltaScale })
-  {
-    const { left, top } = this.state.element.getBoundingClientRect()
-    const { minScale, maxScale, scaleSensitivity } = this.state
-    const [ scale, newScale ] = Renderer.getScale({
-      scale: this.state.transformation.scale,
-      scaleSensitivity,
-      deltaScale,
-      minScale,
-      maxScale
-    })
-
-    const originX = x - left
-    const originY = y - top
-
-    const newOriginX = originX / scale
-    const newOriginY = originY / scale
-
-    const translate = Renderer.getTranslate({ scale, minScale, maxScale })
-
-    const translateX = translate({
-      translate: this.state.transformation.translateX,
-      prevPos: this.state.transformation.originX,
-      pos: originX
-    })
-
-    const translateY = translate({
-      translate: this.state.transformation.translateY,
-      prevPos: this.state.transformation.originY,
-      pos: originY
-    })
-
-    this.state.element.style.transformOrigin = `${newOriginX}px ${newOriginY}px`
-    this.state.element.style.transform = Renderer.getMatrix({
-      scale: newScale,
-      translateX,
-      translateY
-    })
-
-    this.state.transformation = {
-      originX: newOriginX,
-      originY: newOriginY,
-      scale: newScale,
-      translateX,
-      translateY
-    }
-  }
-
-  pan({ originX, originY })
-  {
-    this.state.transformation.translateX += originX
-    this.state.transformation.translateY += originY
-
-    this.state.element.style.transform = Renderer.getMatrix({
-      translateX: this.state.transformation.translateX,
-      translateY: this.state.transformation.translateY,
-      scale: this.state.transformation.scale
-    })
-  }
-
-  panTo({ originX, originY, scale })
-  {
-    this.state.transformation.scale = scale
-
-    this.pan({
-      originX: originX - this.state.transformation.translateX,
-      originY: originY - this.state.transformation.translateY
-    })
-  }
-
-  panBy({ originX, originY })
-  {
-    this.pan({ originX, originY })
-  }
-}
-
 function whichBtn(e, which = 1)
 {
   if (!e.which && e.button) {
@@ -194,13 +74,13 @@ const lightbox = Vue.component('lightbox', {
     },
     panTo()
     {
-      this.renderer.panTo({ originX: 0, originY: 0, scale: 1 })
+      this.$renderer.panTo({ originX: 0, originY: 0, scale: 1 })
     },
     panBy(e)
     {
       e.preventDefault()
 
-      this.renderer.panBy({
+      this.$renderer.panBy({
         originX: e.movementX,
         originY: e.movementY
       })
@@ -223,7 +103,7 @@ const lightbox = Vue.component('lightbox', {
     {
       e.preventDefault()
 
-      this.renderer.zoom({
+      this.$renderer.zoom({
         deltaScale: Math.sign(e.deltaY),
         x: e.clientX,
         y: e.clientY
@@ -287,11 +167,8 @@ const lightbox = Vue.component('lightbox', {
       this.$bus.$on('lightbox:open', this.open)
     }
 
-    this.renderer = Renderer.init({
-      element: this.$refs.previewImg,
-      scaleSensitivity: 50,
-      minScale: .1,
-      maxScale: 30
+    this.$renderer.init({
+      element: this.$refs.previewImg
     })
   }
 })
--- main.js
+++ main.js
@@ -1,3 +1,122 @@
+Vue.prototype.$renderer = (function() {
+  const valueInRange = ({ minScale, maxScale, scale }) => scale <= maxScale && scale >= minScale
+  const hasPositionChanged = ({ pos, prevPos }) => pos !== prevPos
+
+  const getTranslate = ({ minScale, maxScale, scale }) => ({ pos, prevPos, translate }) =>
+    valueInRange({ minScale, maxScale, scale }) &&
+    hasPositionChanged({ pos, prevPos })
+      ? translate + (pos - prevPos * scale) * (1 - 1 / scale)
+      : translate
+
+  const getScale = ({ scale, minScale, maxScale, scaleSensitivity, deltaScale }) => {
+    let newScale = scale + (deltaScale / (scaleSensitivity / scale))
+
+    newScale = Math.max(minScale, Math.min(newScale, maxScale))
+
+    return [scale, newScale]
+  }
+
+  const getMatrix = ({ scale, translateX, translateY, skewX = 0, skewY = 0 }) =>
+    `matrix(${scale}, ${skewX}, ${skewY}, ${scale}, ${translateX}, ${translateY})`
+
+  const defaultOptions = {
+    element: document.documentElement,
+    scaleSensitivity: 50,
+    minScale: .1,
+    maxScale: 30
+  }
+
+  return new class Renderer
+  {
+    constructor(options)
+    {
+      this.state = { transformation: { translateX: 0, translateY: 0, originX: 0, originY: 0, scale: 1 } }
+
+      this.init(options)
+    }
+
+    init(options = {})
+    {
+      Object.assign(this.state, defaultOptions, options)
+    }
+
+    zoom({ x, y, deltaScale })
+    {
+      const { left, top } = this.state.element.getBoundingClientRect()
+      const { minScale, maxScale, scaleSensitivity } = this.state
+      const [ scale, newScale ] = getScale({
+        scale: this.state.transformation.scale,
+        scaleSensitivity,
+        deltaScale,
+        minScale,
+        maxScale
+      })
+
+      const originX = x - left
+      const originY = y - top
+
+      const newOriginX = originX / scale
+      const newOriginY = originY / scale
+
+      const translate = getTranslate({ scale, minScale, maxScale })
+
+      const translateX = translate({
+        translate: this.state.transformation.translateX,
+        prevPos: this.state.transformation.originX,
+        pos: originX
+      })
+
+      const translateY = translate({
+        translate: this.state.transformation.translateY,
+        prevPos: this.state.transformation.originY,
+        pos: originY
+      })
+
+      this.state.element.style.transformOrigin = `${newOriginX}px ${newOriginY}px`
+      this.state.element.style.transform = getMatrix({
+        scale: newScale,
+        translateX,
+        translateY
+      })
+
+      this.state.transformation = {
+        originX: newOriginX,
+        originY: newOriginY,
+        scale: newScale,
+        translateX,
+        translateY
+      }
+    }
+
+    pan({ originX, originY })
+    {
+      this.state.transformation.translateX += originX
+      this.state.transformation.translateY += originY
+
+      this.state.element.style.transform = getMatrix({
+        translateX: this.state.transformation.translateX,
+        translateY: this.state.transformation.translateY,
+        scale: this.state.transformation.scale
+      })
+    }
+
+    panTo({ originX, originY, scale })
+    {
+      this.state.transformation.scale = scale
+
+      this.pan({
+        originX: originX - this.state.transformation.translateX,
+        originY: originY - this.state.transformation.translateY
+      })
+    }
+
+    panBy({ originX, originY })
+    {
+      this.pan({ originX, originY })
+    }
+  }
+})()
+
 Vue.prototype.$fullscreen = (function(fullscreen) {
   const isEnabled = false, fn = {}
 
